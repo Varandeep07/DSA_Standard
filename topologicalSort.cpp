ce61b61b552fed80926f88c589373482b46e5ff1
Varandeep07 <76240587+Varandeep07@users.noreply.github.com>
2022-06-03 09:21:16 +0530

    Create topologicalSort.cpp

---
+++ topologicalSort.cpp
@@ -0,0 +1,33 @@
+	vector<int> topoSort(int n, vector<int> adj[]) 
+	{
+	    // https://practice.geeksforgeeks.org/problems/topological-sort/1#
+	    
+	    vector<int> Indegree(n,0);
+	    for(int i=0; i<n; i++){
+	        for(auto &x: adj[i]){
+	            Indegree[x]++;
+	        }
+	    }
+	    queue<int> q; // find node with indegree 0 and push in the queue 
+	    vector<int> topo_list; // after pop out reduce its child's indegree by 1 
+	    for(int i=0; i<n; i++){ // also find the child with indegree 0 and push in the queue
+	        if(!Indegree[i]){
+	            q.push(i);
+	        }
+	    }
+	    vector<bool> vis(n,false);
+	    while(!q.empty()){
+	        int val = q.front();
+	        q.pop();
+	        vis[val] = true;
+	        topo_list.push_back(val);
+	        for(auto &x: adj[val]){
+	            if(vis[x]) continue;
+	            Indegree[x]--;
+	            if(!Indegree[x]){
+	                q.push(x);
+	            }
+	        }
+	    }
+	    return topo_list;
+	}
